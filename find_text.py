0a12d890bc6487d361e7cac724b20c3759ae7b85
ThePotato456 <thepotato456@yahoo.com>
2021-10-16 16:02:54 -0500

    properly handle keyboardinterrupt exit

--- find_text.py
+++ find_text.py
@@ -23,13 +23,17 @@ def get_text_from_image(img_path, try_count, image_count):
 
 try_count = 0
 if __name__ == "__main__":
-    image_count = len(os.listdir('images'))
-    non_images = []
-    for image in os.listdir('images'):
-        try:
-            try_count = try_count + 1
-            get_text_from_image(os.path.join('images/', image), try_count, image_count)
-            time.sleep(2)
-        except Exception as e:
-            non_images.append(image)
-    print(non_images)
+    try:
+        image_count = len(os.listdir('images'))
+        non_images = []
+        for image in os.listdir('images'):
+            try:
+                try_count = try_count + 1
+                get_text_from_image(os.path.join('images/', image), try_count, image_count)
+                time.sleep(2)
+            except Exception as e:
+                non_images.append(image)
+        print(non_images)
+    except KeyboardInterrupt:
+        print('\n[-] Exiting find-text.py, goodbye!')
+        exit(0)
--- scraper.py
+++ scraper.py
@@ -16,37 +16,43 @@ def generate_link():
     result_str = (''.join((random.choice(source) for i in range(6)))).lower()
     return (result_str)
 
-if not os.path.exists('images'):
-    os.mkdir('images/')
 
-if len(os.listdir('images/')):
-    file_count = (len(os.listdir('images/')))
-else:
-    file_count = 0
+if __name__ == "__main__":
+    try:
+        if not os.path.exists('images'):
+            os.mkdir('images/')
 
-try_count = 0
-while True:
-    try_count = try_count + 1
-    url = "https://prnt.sc/" + generate_link()
-    sys.stdout.write('\r[{}] Trying {}'.format(try_count, url))
-    parser = 'html.parser' 
-    response = requests.get(url, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36"})
-    http_encoding = response.encoding if 'charset' in response.headers.get('content-type', '').lower() else None
-    html_encoding = EncodingDetector.find_declared_encoding(response.content, is_html=True)
-    encoding = html_encoding or http_encoding
-    soup = BeautifulSoup(response.content, parser, from_encoding=encoding)
-    
-    time.sleep(0.01)
-    for image in soup.find_all('img', src=True):
-        if 'https://image.prntscr.com/image/' in image['src']:
-            file_count = file_count + 1
-            print(' -> Image link for {}: {} ({})'.format(url[(len("https://prnt.sc/")):], image['src'], file_count))
-            file_name = os.path.join('images',image['src'][32:])
-            if os.path.exists(file_name):
-                break
-            dl = requests.get(image['src'], stream=True, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36"})
-            with open(file_name, "wb") as image_file:
-                shutil.copyfileobj(BytesIO(dl.content), image_file)
-    sys.stdout.flush()
+        if len(os.listdir('images/')):
+            file_count = (len(os.listdir('images/')))
+        else:
+            file_count = 0
 
-    time.sleep(0.3)
+        try_count = 0
+        while True:
+            try_count = try_count + 1
+            url = "https://prnt.sc/" + generate_link()
+            sys.stdout.write('\r[{}] Trying {}'.format(try_count, url))
+            parser = 'html.parser' 
+            response = requests.get(url, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36"})
+            http_encoding = response.encoding if 'charset' in response.headers.get('content-type', '').lower() else None
+            html_encoding = EncodingDetector.find_declared_encoding(response.content, is_html=True)
+            encoding = html_encoding or http_encoding
+            soup = BeautifulSoup(response.content, parser, from_encoding=encoding)
+            
+            time.sleep(0.01)
+            for image in soup.find_all('img', src=True):
+                if 'https://image.prntscr.com/image/' in image['src']:
+                    file_count = file_count + 1
+                    print(' -> Image link for {}: {} ({})'.format(url[(len("https://prnt.sc/")):], image['src'], file_count))
+                    file_name = os.path.join('images',image['src'][32:])
+                    if os.path.exists(file_name):
+                        break
+                    dl = requests.get(image['src'], stream=True, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36"})
+                    with open(file_name, "wb") as image_file:
+                        shutil.copyfileobj(BytesIO(dl.content), image_file)
+            sys.stdout.flush()
+
+            time.sleep(0.3)
+    except KeyboardInterrupt:
+        print('\n[-] Exiting scraper.py, goodbye!')
+        exit(0)
